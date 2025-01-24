b536a7ceae6ede0157d8d8f13895041bead0a899
gautamsingh75 <gautamrajput608@gmail.com>
2025-01-24 16:34:09 +0530

    Update Footer.jsx

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             {/* <footer className="footer footer-center p-4 bg-base-300 text-base-content"> */}
             <footer className="footer footer-center p-4 bg-neutral text-white">
                 <aside>
-                    <p>Copyright © 2024 - All right reserved by BloodBridge Ltd</p>
+                    <p>Copyright © 2025 - All right reserved by BloodBridge Ltd</p>
                 </aside>
             </footer>
         </div>
